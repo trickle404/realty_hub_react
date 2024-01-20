08a90b7a96caf132d0f3cd1bcb946b8d4852e4be
GoNorman <dr.cheif-afanasyev@yandex.ru>
2024-01-20 10:51:18 +0100

    start

--- src/component/public/HomePage.jsx
+++ src/component/public/HomePage.jsx
@@ -19,7 +19,7 @@ const HomePage = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:8090/public/home');
+        const response = await axios.get('http://realty-hub-backend-b2a57ab30fb8.herokuapp.com/public/home');
         setData(response.data);
         setDataResponse(true);
       } catch (error) {
